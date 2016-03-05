43a3757cb7c39a3ad9f21437ff6ba8333c461fcc
yintothayang <yintothayang@gmail.com>
2016-03-05 00:21:18 -0800

    sent switch

---
+++ src/components/sentSwitch/sentSwitch.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sent-switch" @click.stop="">
+    <div class="switch">
+      <label>
+        <input type="checkbox" v-model="route.attributes.sent"  @click.stop="flipSwitch">
+        <span class="lever"></span>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+ import RouteModel from '../../models/RouteModel.js'
+ import UserModel from '../../models/UserModel.js'
+ export default {
+   name: 'SentSwitch',
+   props: ['route'],
+   data(){
+     return {
+       switchFlipped: true
+     }
+   },
+   created(){
+
+   },
+
+   methods: {
+     flipSwitch(){
+       if(this.route.attributes.sent){
+         RouteModel.sendRoute(route, UserModel.currentUser).then(results => {
+           console.log(results);
+         });
+       }
+     }
+   }
+ }
+</script>
+
+<style lang="sass">
+ .sent-switch {
+
+ }
+</style>
