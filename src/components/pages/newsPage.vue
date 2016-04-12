ab3734c744be4835f4b746894f0b757884d31731
yintothayang <yintothayang@gmail.com>
2016-04-11 23:54:49 -0700

    started news page

---
+++ src/components/pages/newsPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="news-page">
+
+  </div>
+</template>
+<script>
+ import WallModel from '../../models/WallModel.js'
+ import RouteModel from '../../models/RouteModel.js'
+
+ var NewsPage = BaseComponent.extend({
+   name: 'NewsPage',
+   data(){
+     return {
+
+     }
+   },
+   components: {
+
+   },
+   created(){
+     this.notifications.notify('Navbar.setHeader', "News");
+   },
+   ready(){
+
+   },
+   methods: {
+     getNews(){
+
+     }
+   }
+ });
+
+ export default GymLayoutPage
+</script>
+
+<style lang="sass">
+ @import '../../styles/main.scss';
+
+ .news-page {
+   display: flex;
+   flex-wrap: wrap;
+
+
+ }
+</style>
