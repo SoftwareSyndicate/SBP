219d520f6476c12542fa22530baa10a5ba397088
yintothayang <yintothayang@gmail.com>
2016-04-12 00:06:49 -0700

    news list

---
+++ src/components/newsList/newsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="news-list">
+
+  </div>
+</template>
+
+<script>
+ import BaseComponent from '../base/baseComponent.vue'
+
+ var NewsList = BaseComponent.extend({
+   name: 'NewsList',
+   data(){
+     return {
+
+     }
+   },
+   created(){
+
+   },
+
+   methods: {
+     calculateGradeTotals(routes){
+
+     }
+   }
+ });
+
+ export default NewsList;
+</script>
+
+<style lang="sass">
+ $cell-padding: 1.4em;
+ $cell-border: 2px rgba(215, 218, 230, .5) solid;
+
+ .news-list-table {
+   display: flex;
+   flex-wrap: wrap;
+   flex-grow: 1;
+   background-color: white;
+   color: rgba(0, 0, 0, .5);
+ }
+</style>
---
+++ src/components/newsList/newsListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="news-list-item">
+
+  </div>
+</template>
+
+<script>
+ export default {
+   name: 'NewsListItem',
+   data(){
+     return {
+
+     }
+   },
+   created(){
+
+   },
+   methods: {
+     calcColorPercents(){
+
+     }
+   },
+ }
+</script>
+
+<style lang="sass">
+ @import '../../styles/main.scss';
+
+ .news-list-item {
+   padding: 0 0 0 10px !important;
+   display: flex;
+   background-color: white;
+   flex-grow: 1;
+
+
+ }
+</style>
